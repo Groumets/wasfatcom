1bb7a71ffa7a062f8e7f3ad693297e7ad375d4f2
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-04-22 10:44:01 +0300

    done 2

--- server.js
+++ server.js
@@ -125,6 +125,56 @@ app.post('/search', (req, res) => {
 });
 
 
+
+
+// yasmeen work 
+app.get('/login', loginPage);
+app.get('/BMI', setInfo);
+app.get('/search', setSearch);
+app.get('/',indexPage);
+app.post('/test', bmi);
+
+
+function indexPage(req, response) {
+  response.render('index');
+}
+
+function loginPage(req, response) {
+    response.render('login');
+
+}
+function setInfo(req, response) {
+    response.render('info');
+}
+function setSearch(req, response) {
+    response.render('search');
+
+}
+
+function bmi(req, response) {
+    let weightForm = (req.body.weight)
+    let heightForm = (req.body.height)
+    console.log(`${weightForm} , ${heightForm} this is`);
+
+    var unirest = require("unirest");
+    var req = unirest("GET", "https://gabamnml-health-v1.p.rapidapi.com/bmi");
+    req.query({
+        "height": heightForm,
+        "weight": weightForm
+
+    });
+    req.headers({
+        "x-rapidapi-host": "gabamnml-health-v1.p.rapidapi.com",
+        "x-rapidapi-key": process.env.X_RAPIDAPI_KEY
+    });
+    req.end(function (res) {
+        if (res.error) throw new Error(res.error);
+        console.log(res.body);
+        response.render('test', { obj: res.body });
+    });
+
+}
+
 function Recipes(details){
   this.title=details.label ? details.label : 'undifed' ;
   this.imgurl=details.image ? details.image : 'undifed';
